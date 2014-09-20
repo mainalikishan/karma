445f51e363a81533d26c61cd5daebc6d31759ea7
unknown <thebhandariprakash@gmail.com>
2014-09-20 12:24:35 +0545

    coporate user registration error corrected

---
+++ app/Karma/Registration/CopUserRegister.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Prakash
+ * Date: 9/20/14
+ * Time: 12:19 PM
+ */
+
+namespace Karma\Registration;
+
+
+class CopUserRegister {
+
+    public function checkRegistration($post)
+    {
+        return "hi ";
+    }
+
+} 
--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,7 @@
 Route::group(['prefix' => 'api/v1'], function(){
     // Individual User routes
     Route::get('induser/login', 'IndUserController@login');
+    Route::get('copuser/register', 'CopUserRegisterController@register');
 
     // Corporate User routes
 });
