a1a18baa8a5894153aa053bd1ac80abff0b5ea8b
Kishan Mainali <mainalikishan@gmail.com>
2014-09-29 17:23:02 +0545

    something updated

---
+++ app/Karma/General/Country.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kishan
+ * Date: 9/29/14
+ * Time: 2:12 PM
+ */
+
+namespace Karma\General;
+
+
+class Country {
+
+} 
---
+++ app/Karma/General/Gender.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kishan
+ * Date: 9/29/14
+ * Time: 1:19 PM
+ */
+
+namespace Karma\General;
+
+
+class Gender {
+
+} 
